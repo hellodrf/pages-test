b6222b78525d018b12c9064faa2c1c82223d0af6
Aaron Du <aaronduooo@gmail.com>
2021-05-17 17:00:34 +0800

    initial

---
+++ precache-manifest.fc9b89f32138680bb3a3acf1de55a5eb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ce0e94503528f10e329b89eddf95d36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "579807c27e1140ec4791",
+    "url": "/static/css/2.9e0cac87.chunk.css"
+  },
+  {
+    "revision": "788f8369cf1ecd434021",
+    "url": "/static/css/main.36fd9483.chunk.css"
+  },
+  {
+    "revision": "579807c27e1140ec4791",
+    "url": "/static/js/2.58673f47.chunk.js"
+  },
+  {
+    "revision": "46aedefcd19e23dff04c81b1a074f8d8",
+    "url": "/static/js/2.58673f47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "788f8369cf1ecd434021",
+    "url": "/static/js/main.9663be2c.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "80aeb2c552aba18518b679ede1f64ff5",
+    "url": "/static/media/RS-UnimelbLogo.80aeb2c5.svg"
+  },
+  {
+    "revision": "10edce5b96b41673c3c14519a0fdb88a",
+    "url": "/static/media/background.10edce5b.jpg"
+  },
+  {
+    "revision": "8784cb962b0593b5efb51100b12ec2ed",
+    "url": "/static/media/card-image.8784cb96.jpg"
+  }
+]);
